087476b9a9144f173369dcf7daccb92094704e20
Jonatas Ferreira <silvajonatas2@gmail.com>
2022-01-27 20:28:21 -0300

    Memo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -19,3 +19,17 @@ export function SearchResults({ results }: SearchResultsProps){
     </div>
   );
 }
+
+/**
+ * When does a react component render again?
+ * 1 - Create a new component version
+ * 2 - Compare with the previous version
+ * 3 - If there are changes, it will update what was changed.
+ * 
+ * ----------------------------------------------
+ * In which situations is it recommended to use Memo?
+ * 1 - Pure Functional Components
+ * 2 - Renders too often
+ * 3 - Re-renders with same prevProps
+ * 4 - Medium to big size
+ */
