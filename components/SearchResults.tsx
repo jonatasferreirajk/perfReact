5cddfbfebe7403393d06dcab14f2a2dd59bcde27
Jonatas Ferreira <silvajonatas2@gmail.com>
2022-01-28 18:42:05 -0300

    useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,20 +1,27 @@
+import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
-interface SearchResultsProps{
+interface SearchResultsProps {
   results: Array<{
-    id: number,
-    price: number,
-    title: string
-  }>
+    id: number;
+    price: number;
+    title: string;
+  }>;
 }
 
-export function SearchResults({ results }: SearchResultsProps){
+export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
-      {results.map(product => {
-        return(
-          <ProductItem key={product.id} product={product} />
-        );
+      <h2>{totalPrice}</h2>
+
+      {results.map((product) => {
+        return <ProductItem key={product.id} product={product} />;
       })}
     </div>
   );
@@ -25,11 +32,13 @@ export function SearchResults({ results }: SearchResultsProps){
  * 1 - Create a new component version
  * 2 - Compare with the previous version
  * 3 - If there are changes, it will update what was changed.
- * 
+ *
  * ----------------------------------------------
  * In which situations is it recommended to use Memo?
  * 1 - Pure Functional Components
  * 2 - Renders too often
  * 3 - Re-renders with same prevProps
  * 4 - Medium to big size
+ *-----------------------------------------------
+ * useMemo / useCallback
  */
